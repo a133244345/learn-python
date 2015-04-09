81a413c650c2b35baeccc44abe2a3b35d78eb39a
a133244345 <133244345@qq.com>
2015-04-09 22:25:12 +0800

    通风一班路琦的作业

---
+++ test1-1.py
@@ -0,0 +1,23 @@
+# -*- encoding:utf-8 -*-
+s1='姓名：路琦'
+s2='学号：1403050112'
+s3='班级：通风一班'
+# -*- encoding:utf-8 -*-
+
+a1='夜空中最亮的星'
+a2='能否听清'
+a3='那仰望的人'
+a4='心中的孤独'
+a5='夜空中最亮的星'
+a6='能否记起'
+a7='曾与我同行'
+a8='消失在风里的身影'
+
+print a1.decode ('utf-8')
+print a2.decode ('utf-8')
+print a3.decode ('utf-8')
+print a4.decode ('utf-8')
+print a5.decode ('utf-8')
+print a6.decode ('utf-8')
+print a7.decode ('utf-8')
+print a8.decode ('utf-8')
---
+++ test1-2.py
@@ -0,0 +1,19 @@
+# -*- encoding:utf-8 -*-
+s1='姓名：路琦'
+s2='班级：通风一班'
+s3='学号：1403050112'
+# -*- encoding:utf-8 -*-
+
+import math
+f1=(math.exp(3*0.1)-math.exp(3))/0.1
+print f1
+f2=(math.exp(3*0.01)-math.exp(3))/0.01 
+print f2
+f3=(math.exp(3*0.001)-math.exp(3))/0.001
+print f3
+f4=(math.exp(3*0.0001)-math.exp(3))/0.0001
+print f4
+f5=(math.exp(3*0.000001)-math.exp(3))/0.00001
+print f5
+
+# -*- encoding:utf-8 -*-
